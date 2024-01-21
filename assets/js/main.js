d911947ec3e1eecb36930db5fe62cacbea223642
Ahmed Assem <ahmedasem224@gmail.com>
2024-01-21 19:51:29 +0200

    Games OOP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,66 @@
-const searchInput = document.querySelector('.search__input input');
-const citiesList = document.querySelectorAll('.city__example ul li');
-const currentTemp = document.querySelector('.current__temp span');
-const currentCity = document.querySelector('.city__time .city span');
-const currentTime = document.querySelector('.city__time .time span');
-const currentCondition = document.querySelector('.info__icon .info span ');
-const currentImg = document.querySelector('.info__icon img');
-const secondImg = document.querySelectorAll('.day img');
-const secondTemp = document.querySelectorAll('.day .temp');
-const secondTime = document.querySelectorAll('.day .time');
-const secondCondition = document.querySelectorAll('.condition__text');
-const weatherDetails = document.querySelectorAll('.weather__details li span')
 
-let weatherResponse;
-
-(async function() {
-    let weatherApi = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4b2610a0b9ea4edebf6171529232912&q=egypt&days=3`)
-    weatherResponse = await weatherApi.json()
-    displayWeather()
-})()
-
-async function fetchWeather() {
-    let weatherApi = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4b2610a0b9ea4edebf6171529232912&q=${searchInput.value}&days=3`)
-    weatherResponse = await weatherApi.json()
-    displayWeather()
-}
+// const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': 'f933d125bdmsh8ba28456d8d82a2p1bbf89jsn473451910cbb',
+//         'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+//     }
+// };
+// (async function () {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     console.log(result);
+// })()
 
 
-searchInput.addEventListener('keyup', function () {
-    fetchWeather()
+
+
+
+
+
+// ********** loading ***********
+// *** الفانكشن ديه بتستنى لحد ما كل البانات و الصور تحمل
+// window.addEventListener('load', function () {
+//     $('.loadingScreen').fadeOut(500, function () {
+//         $('body').css('overflow', 'auto')
+//     })
+// })
+// *** الفانكشن ديه بتشتغل لما الكود يتقرأ فقط لكن مش بتستنى لما الصور تحمل
+$(document).ready(function(){
+    $('.loadingScreen').fadeOut(500, function () {
+        $('body').css('overflow', 'auto')
+    })
 })
 
-for (const city of citiesList) {
-    city.addEventListener('click', function (e) {
-        searchInput.value = e.target.textContent
-        fetchWeather()
+
+// ---------------------
+const links = document.querySelectorAll(".nav-link")
+// const gamePage = document.querySelector(".games__card .row")
+
+// ********** class active *********
+for (const cate of links) {
+    cate.addEventListener('click', function () {
+        for (const active of links) {
+            active.classList.remove("active")
+        }
+        cate.classList.add("active")
     })
 }
 
-function displayWeather() {
-    currentTemp.innerHTML = `${weatherResponse.current.temp_c}<sup>o</sup>c`
-    currentCity.innerHTML = weatherResponse.location.country
-    currentTime.innerHTML = weatherResponse.forecast.forecastday[0].date
-    currentImg.setAttribute('src', weatherResponse.current.condition.icon)
-    currentCondition.innerHTML = weatherResponse.current.condition.text
-    secondImg[0].setAttribute('src', weatherResponse.forecast.forecastday[1].day.condition.icon)
-    secondImg[1].setAttribute('src', weatherResponse.forecast.forecastday[2].day.condition.icon)
-    secondTemp[0].innerHTML = `${weatherResponse.forecast.forecastday[1].day.avgtemp_c}<sup>o</sup>c`
-    secondTemp[1].innerHTML = `${weatherResponse.forecast.forecastday[2].day.avgtemp_c}<sup>o</sup>c`
-    secondTime[0].innerHTML = weatherResponse.forecast.forecastday[1].date
-    secondTime[1].innerHTML = weatherResponse.forecast.forecastday[2].date
-    secondCondition[0].innerHTML = weatherResponse.forecast.forecastday[1].day.condition.text
-    secondCondition[1].innerHTML = weatherResponse.forecast.forecastday[2].day.condition.text
-    weatherDetails[0].innerHTML = weatherResponse.current.cloud + "%"
-    weatherDetails[1].innerHTML = weatherResponse.current.humidity + "%"
-    weatherDetails[2].innerHTML = weatherResponse.current.wind_kph + "km/h"
-    console.log(weatherResponse);
+
+import { DisplayGames } from "./game-module-display.js"
+
+const cateName = new DisplayGames("mmorpg")
+cateName.display()
+
+
+
+for (const cate of links) {
+    cate.addEventListener("click", function () {
+        const val = cate.innerText
+        cateName.category = val
+        cateName.display()
+    })
 }
+
